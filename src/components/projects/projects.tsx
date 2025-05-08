cad97ac659ff1818ea3aa3b81d7f1fdc600fe470
Tiago Silva <tiagosilva_live_2016@outlook.com>
2025-05-08 18:00:01 -0300

    doc(componets-projects): add images to assets, the imported in projects, create code of projects

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,3 +1,9 @@
+import AppTempo from "../../assets/app.png";
+import Pokemon from "../../assets/gif.gif";
+import Formulario from "../../assets/formulario.png";
+import FetchGitHUb from "../../assets/gif.projeto-Fetch-GitHub.gif"
+import { Link } from "react-router-dom";
+
 export const Projects = () => {
     return (
         <div>
@@ -5,10 +11,50 @@ export const Projects = () => {
 
             <div>
                 <h2>Previsão do tempo</h2>
+                <Link to="https://github.com/Tiago-SilvaRP/App-previsao-do-tempo" target="_blanck" rel="noopener noreferrer">🔗 Ver repositório</Link>
+                <img src={AppTempo} alt="Imagem da interface do projeto Previsão do tempo" />
                 <div>
                     <p>Projeto feito com HTML, CSS, JavaScript e consumo de API</p>
+                    <Link to="https://tiago-silvarp.github.io/App-previsao-do-tempo/" target="_blank"
+                        rel="noopener noreferrer">🔗 Ver no GitHub Pages</Link>
                 </div>
             </div>
+
+            <div>
+                <h2>Pokemon</h2>
+                <Link to="https://github.com/Tiago-SilvaRP/pokemon-react" target="_blanck" rel="noopener noreferrer">🔗 Ver repositório</Link>
+                <img src={Pokemon} alt="Gif de funcionamento da página Pokemon" />
+                <div>
+                    <p>Projeto feito com HTML, CSS, JavaScript, consumo de API, React, Vite e Context-api</p>
+                    <Link to="https://pokemon-react-snowy-omega.vercel.app/" target="_blank" rel="noopener noreferrer">🔗 Ver
+                        no GitHub Pages</Link>
+                </div>
+            </div>
+
+            <div>
+                <h2>Formulário de validação</h2>
+                <Link to="https://github.com/Tiago-SilvaRP/formularioDeValidacao" target="_blanck" rel="noopener noreferrer">🔗 Ver repositório</Link>
+                <img src={Formulario} alt="Imagem da interface do projeto de validação de formulário" />
+
+                <div>
+                    <p>Projeto feito com HTML, CSS e JavaScript.</p>
+                    <Link to="https://tiago-silvarp.github.io/formularioDeValidacao/" target="_blank"
+                        rel="noopener noreferrer">🔗 Ver no GitHub Pages</Link>
+                </div>
+            </div>
+
+            <div>
+                <h2>API Fetch GitHub</h2>
+                <Link to="https://github.com/Tiago-SilvaRP/Projeto--Fetch-GitHub" target="_blanck" rel="noopener noreferrer">🔗 Ver repositório</Link>
+                <img src={FetchGitHUb} alt="Gif com demonstração de funcionamento da pagina." />
+
+                <div>
+                    <p>Projeto feito com HTML, CSS, JavaScript e consumo de API</p>
+                    <Link to="https://tiago-silvarp.github.io/Projeto--Fetch-GitHub/">🔗 Ver no GitHub Pages</Link>
+                
+                </div>
+            </div>
+
         </div>
     )
 }
